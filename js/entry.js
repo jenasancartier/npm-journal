14017ee361f2017b5e01f9951f736e05579a62b6
Jena SanCartier and Zachary Matthewstearn <zachary@>
2016-10-10 16:16:53 -0700

    finish assignment

--- js/entry.js
+++ js/entry.js
@@ -3,4 +3,50 @@ function Entry(_title, _body){
   this.body = _body;
 }
 
+Entry.prototype.getWordCount = function(){
+  var wordArray = this.title.split(" ").concat(this.body.split(" "));
+  var wordCount = wordArray.length;
+  return wordCount;
+};
+
+Entry.prototype.getLetterTypesCount = function(){
+  var letterArray = (this.title.toLowerCase() + this.body.toLowerCase());
+  var vowelCount = 0;
+  var consonantCount = 0;
+  for(var i = 0; i < letterArray.length; i++){
+    if(letterArray.charAt(i) === 'a' ||
+       letterArray.charAt(i) === 'e' ||
+       letterArray.charAt(i) === 'i' ||
+       letterArray.charAt(i) === 'o' ||
+       letterArray.charAt(i) === 'u')
+      vowelCount++;
+    else
+      consonantCount++;
+  }
+  return "Vowels: " + vowelCount + " | Consonants: " + consonantCount;
+};
+
+Entry.prototype.getTeaser = function(){
+  var firstSentence = this.body.split(" ");
+  var output = "";
+  var index = 0;
+  var sentenceDone = false;
+  while(index < 8 && index < firstSentence.length && !sentenceDone){
+    console.log("In the loop!");
+    output += firstSentence[index];
+    if(firstSentence[index].endsWith(".") ||
+       firstSentence[index].endsWith("!") ||
+       firstSentence[index].endsWith("?")){
+      sentenceDone = true;
+    }
+    else
+      output += " ";
+
+    index++;
+  }
+  if(!sentenceDone)
+    output += "...";
+  return output;
+};
+
 exports.entryModule = Entry;
--- js/journal-interface.js
+++ js/journal-interface.js
@@ -5,7 +5,6 @@ $("#input_newEntry").submit(function(event){
   var newTitle = $("#title").val();
   var newBody = $("#body").val();
   var newEntry = new Entry(newTitle, newBody);
-
-  $("#entries-log").prepend("<hr><li><h3>" + newEntry.title + "</h3><p>" + newEntry.body + "</p></li>");
+  $("#entries-log").prepend("<hr><li><h3>" + newEntry.title + "</h3><p>" + newEntry.getTeaser() + "<br>[<em>Words: " + newEntry.getWordCount() + " | " + newEntry.getLetterTypesCount() + "</em>]</p><p>" + newEntry.body + "</p></li><br>");
   $("#entries-title").show();
 });
